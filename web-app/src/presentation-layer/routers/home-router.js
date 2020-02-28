6f630b46aeffde902bbf03dde16816ae51d8bc87
J <99johanbogg@gmail.com>
2020-02-28 22:42:45 +0100

    improved search page

--- web-app/src/presentation-layer/routers/home-router.js
+++ web-app/src/presentation-layer/routers/home-router.js
@@ -12,29 +12,38 @@ router.get('/', function(request, response) {
 router.get('/search', function(request, response) {
 
     var type = request.query.type
+    const search = request.query.q
 
     if (type == null || type == '') {
         type = 'recruiter'
     }
 
-    internshipManager.searchAdverts(type, request.query.q, function(status, advertsOrError) {
-        if (status) {
-            var model = {
-                sendMessageHidden: true,
-                Posts: advertsOrError,
-                search: request.query.q
-            }
-            if (type == 'recruiter') {
-                response.render("internship/recruiter-adverts.hbs", model)
-            } else if (type == 'student') {
-                response.render("internship/student-adverts.hbs", model)
-            } else {
-                response.render('errors/500.hbs', {validationErrors: 'Wrong type submitted'})
-            }
+    if (search == null || search == '') {
+        if (type == 'recruiter') {
+            response.redirect('/positions')
         } else {
-            response.render('errors/500.hbs', {validationErrors: advertsOrError})
+            response.redirect('/student-adverts')
         }
-    })
+    } else {
+        internshipManager.searchAdverts(type, search, function(status, advertsOrError) {
+            if (status) {
+                var model = {
+                    sendMessageHidden: true,
+                    Posts: advertsOrError,
+                    search: request.query.q
+                }
+                if (type == 'recruiter') {
+                    response.render("internship/recruiter-adverts.hbs", model)
+                } else if (type == 'student') {
+                    response.render("internship/student-adverts.hbs", model)
+                } else {
+                    response.render('errors/500.hbs', {validationErrors: 'Wrong type submitted'})
+                }
+            } else {
+                response.render('errors/500.hbs', {validationErrors: advertsOrError})
+            }
+        })
+    }
 })
 
 module.exports = router
